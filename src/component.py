c52930a7df4bffcdfef89805e04b7940f981a94d
Adam Bako <bakoad@gmail.com>
2021-06-08 17:41:00 +0200

    inital commit

---
+++ src/component.py
@@ -0,0 +1,116 @@
+'''
+Template Component main class.
+
+'''
+import csv
+import logging
+import dateparser
+import os
+
+from google_ad_manager.ad_manager_client import GoogleAdManagerClient
+from keboola.utils.header_normalizer import get_normalizer, NormalizerStrategy
+from keboola.component.base import ComponentBase, UserException
+
+# Deprecation = February 2022
+# Sunset = May 2022
+API_VERSION = "v202105"
+
+KEY_CLIENT_EMAIL = "client_email"
+KEY_PRIVATE_KEY = "#private_key"
+KEY_TOKEN_URI = "token_uri"
+KEY_NETWORK_CODE = "network_code"
+KEY_REPORT_TYPE = "report_type"
+KEY_REPORT_NAME = "report_name"
+KEY_METRICS = "metrics"
+KEY_DIMENSIONS = "dimensions"
+KEY_DIMENSION_ATTRIBUTES = "dimension_attributes"
+KEY_TIMEZONE = "timezone"
+KEY_START_DATE = "start_date"
+KEY_END_DATE = "end_date"
+KEY_REPORT_CURRENCY = "report_currency"
+
+REQUIRED_PARAMETERS = []
+REQUIRED_IMAGE_PARS = []
+
+types = ["Historical", "Future sell - through", "Reach", "Sales", "Ad Exchange", "Ad speed"]
+
+
+class Component(ComponentBase):
+    def __init__(self):
+        super().__init__(required_parameters=REQUIRED_PARAMETERS,
+                         required_image_parameters=REQUIRED_IMAGE_PARS)
+
+    def run(self):
+        params = self.configuration.parameters
+
+        client_email = params.get(KEY_CLIENT_EMAIL)
+        private_key = params.get(KEY_PRIVATE_KEY)
+        token_uri = params.get(KEY_TOKEN_URI)
+        network_code = params.get(KEY_NETWORK_CODE)
+        report_type = params.get(KEY_REPORT_TYPE)
+        report_name = params.get(KEY_REPORT_NAME)
+        metrics = params.get(KEY_METRICS)
+        dimensions = params.get(KEY_DIMENSIONS)
+        # dimension_attributes = params.get(KEY_DIMENSION_ATTRIBUTES)
+        timezone = params.get(KEY_TIMEZONE)
+        start_date = params.get(KEY_START_DATE)
+        end_date = params.get(KEY_END_DATE)
+
+        start_date = dateparser.parse(start_date).date()
+        end_date = dateparser.parse(end_date).date()
+
+        client = GoogleAdManagerClient(client_email, private_key, token_uri, network_code, API_VERSION)
+
+        if report_type == "Historical":
+            report_query = client.get_report_query(dimensions, metrics, timezone, start_date=start_date,
+                                                   end_date=end_date)
+        elif report_type == "Ad Exchange historical":
+            report_query = client.get_report_query(dimensions, metrics, timezone, start_date=start_date,
+                                                   end_date=end_date)
+        elif report_type == "Future sell - through":
+            report_query = client.get_report_query(dimensions, metrics, timezone, start_date=start_date,
+                                                   end_date=end_date)
+        elif report_type == "Reach":
+            report_query = client.get_report_query(dimensions, metrics)
+        elif report_type == "Ad speed":
+            report_query = client.get_report_query(dimensions, metrics)
+
+        result_file = client.fetch_report_result(report_query)
+        filesize = os.path.getsize(result_file.name)
+        if filesize == 0:
+            raise UserException("No data found")
+
+        table = self.create_out_table_definition(report_name,
+                                                 incremental=False)
+        columns = self.write_results_get_columns(result_file.name, table.full_path)
+        columns = self.normalize_column_names(columns)
+        table.columns = columns
+
+        self.write_tabledef_manifest(table)
+
+    @staticmethod
+    def normalize_column_names(output_columns):
+        header_normalizer = get_normalizer(strategy=NormalizerStrategy.DEFAULT, forbidden_sub="_")
+        return header_normalizer.normalize_header(output_columns)
+
+    @staticmethod
+    def write_results_get_columns(in_table_path, out_table_path):
+        with open(in_table_path, mode="r") as in_file:
+            reader = csv.DictReader(in_file)
+            with open(out_table_path, mode='wt', encoding='utf-8', newline='') as out_file:
+                writer = csv.DictWriter(out_file, reader.fieldnames)
+                for result in reader:
+                    writer.writerow(result)
+                return reader.fieldnames
+
+
+if __name__ == "__main__":
+    try:
+        comp = Component()
+        comp.run()
+    except UserException as exc:
+        logging.exception(exc)
+        exit(1)
+    except Exception as exc:
+        logging.exception(exc)
+        exit(2)
---
+++ src/google_ad_manager/ad_manager_client.py
@@ -0,0 +1,102 @@
+import sys
+import logging
+import yaml
+import json
+import tempfile
+from googleads import ad_manager
+from googleads import errors
+from googleads.common import ZeepServiceProxy
+
+
+class GoogleAdManagerClient:
+
+    def __init__(self, client_email, private_key, token_uri, network_code, api_version):
+        logging.info(f"Using version of Google Ad Manager API: {api_version}")
+
+        private_key_file = self.get_private_key_file(private_key, client_email, token_uri)
+
+        self.client = self.get_client(network_code, private_key_file)
+        self.report_downloader = self.client.GetDataDownloader(version=api_version)
+
+    @staticmethod
+    def get_client(network_code, private_key_file):
+        try:
+            client = ad_manager.AdManagerClient.LoadFromString(yaml.dump({
+                "ad_manager": {
+                    "application_name": "kds-team.ex-google-ad-manager",
+                    "network_code": network_code,
+                    "path_to_private_key_file": private_key_file
+                }
+            }))
+        except ValueError as e:
+            raise ValueError(
+                f"{e} Please, check format of your private key. New lines"
+                f" must be delimited by \\n character."
+            )
+        client.cache = ZeepServiceProxy.NO_CACHE
+        return client
+
+    @staticmethod
+    def get_private_key_file(private_key, client_email, token_uri) -> str:
+        file_path = "/tmp/private_key.json"
+        with open(file_path, 'w') as outfile:
+            json.dump({
+                "private_key": private_key,
+                "client_email": client_email,
+                "token_uri": token_uri
+            }, outfile)
+        return file_path
+
+    @staticmethod
+    def get_report_query(dimensions, metrics, timezone, dimension_attributes="", ad_unit_view="", currency="",
+                         start_date="", end_date=""):
+        report_query = {
+            'dimensions': dimensions,
+            'columns': metrics,
+            'timeZoneType': timezone
+        }
+
+        if start_date and end_date:
+            report_query['dateRangeType'] = "CUSTOM_DATE"
+            report_query['startDate'] = start_date
+            report_query['endDate'] = end_date
+
+        if dimension_attributes:
+            report_query['dimensionAttributes'] = dimension_attributes
+
+        if ad_unit_view:
+            report_query['adUnitView'] = ad_unit_view
+
+        if currency:
+            report_query['adxReportCurrency'] = currency
+
+        return report_query
+
+    def fetch_report_result(self, report_query):
+        report_file = tempfile.NamedTemporaryFile(
+            mode='w+b', suffix='.csv', delete=False
+        )
+        report_job = {
+            'reportQuery': report_query
+        }
+        print("[INFO]: Create the report")
+        report_job_id = self.create_report(report_job)
+
+        print("[INFO]: Download the report")
+        self.report_downloader.DownloadReportToFile(
+            report_job_id=report_job_id,
+            export_format='CSV_DUMP',
+            outfile=report_file,
+            use_gzip_compression=False
+        )
+        return report_file
+
+    def create_report(self, report_job: dict):
+        """Create report via API"""
+        try:
+            # Run the report and wait for it to finish
+            report_job_id = self.report_downloader.WaitForReport(report_job)
+            return report_job_id
+        except errors.AdManagerReportError as e:
+            print('[INFO]: Failed to generate report. Error: %s' % e)
+            sys.exit()
